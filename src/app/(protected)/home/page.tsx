77227a8707a6ffe7c2519b7c1cf826084abfc334
justindal <justindaludado@cmail.carleton.ca>
2024-06-22 01:10:13 -0400

    new post button added to ui

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -4,8 +4,9 @@ import HomeNav from '@/components/homenavigation'
 import HomeSuggestedCard from '@/components/suggestedcard'
 import Feed from '@/components/feed'
 import Panel from '@/components/userpanel'
-
 import { Separator } from '@/components/ui/separator'
+
+import NewPostButton from '@/components/newpostbutton'
 
 export default async function Page() {
   const session = await auth()
@@ -32,6 +33,7 @@ export default async function Page() {
             <Panel></Panel>
           </div>
         </div>
+        <NewPostButton></NewPostButton>
       </main>
     </div>
   )
