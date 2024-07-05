ed1310c40015642f4ae755b77af1363799d59f01
justindal <justindaludado@cmail.carleton.ca>
2024-07-05 03:32:48 -0400

    register basic values

--- src/components/registerdialog.tsx
+++ src/components/registerdialog.tsx
@@ -15,12 +15,18 @@ import {
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 interface RegisterDialogProps {
   trigger?: React.ReactNode
   isOpenInitial?: boolean
 }
 
-const RegisterDialog = ({ trigger, isOpenInitial = true }: RegisterDialogProps) => {
+const RegisterDialog = ({
+  trigger,
+  isOpenInitial = true,
+}: RegisterDialogProps) => {
   const [isOpen, setIsOpen] = useState(!trigger ? isOpenInitial : false)
 
   useEffect(() => {
@@ -29,40 +35,44 @@ const RegisterDialog = ({ trigger, isOpenInitial = true }: RegisterDialogProps) 
     }
   }, [isOpenInitial, trigger])
 
+  const onSubmit = () => {
+
+  }
+
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       {trigger && <DialogTrigger asChild>{trigger}</DialogTrigger>}
       <DialogContent className='sm:max-w-[425px]'>
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Create New User</DialogTitle>
           <DialogDescription>
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
         </DialogHeader>
         <div className='grid gap-4 py-4'>
           <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor='name' className='text-right'>
-              Name
-            </Label>
-            <Input
-              id='name'
-              defaultValue='Pedro Duarte'
-              className='col-span-3'
-            />
-          </div>
-          <div className='grid grid-cols-4 items-center gap-4'>
             <Label htmlFor='username' className='text-right'>
               Username
             </Label>
             <Input
               id='username'
-              defaultValue='@peduarte'
+              defaultValue='johndoe'
+              className='col-span-3'
+            />
+          </div>
+          <div className='grid grid-cols-4 items-center gap-4'>
+            <Label htmlFor='name' className='text-right'>
+              Name
+            </Label>
+            <Input
+              id='name'
+              defaultValue='John Doe'
               className='col-span-3'
             />
           </div>
         </div>
         <DialogFooter>
-          <Button type='submit'>Save changes</Button>
+          <Button type='submit'>Create Account</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
