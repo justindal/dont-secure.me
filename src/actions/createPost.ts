841b9cff1db4c4dfc481aabcc402b0993197dc67
justindal <justindaludado@cmail.carleton.ca>
2024-07-22 03:31:19 -0400

    server action for post creation added

--- src/actions/createPost.ts
+++ src/actions/createPost.ts
@@ -1,13 +1,25 @@
 'use server'
 
 import db from '@/lib/db'
+import { User, Session } from 'next-auth'
 
 interface Post {
   title?: string
   description?: string
-  user: string
+  user: User
 }
 
-const createPost = async (post: Post) => {}
+const createPost = async ({ title, description, user }: Post) => {
+  const client = await db.clientPromise
+  const database = client.db('tester')
+  const posts = database.collection('posts')
+  const post = await posts.insertOne({
+    title,
+    description,
+    user: user._id,
+    date: new Date(),
+  })
+  return post
+}
 
 export default createPost
