ac49c248575fdb8103636f636d64ef0e3c822ea7
justindal <justindaludado@cmail.carleton.ca>
2024-07-27 05:18:02 -0400

    upload image server action

---
+++ src/actions/uploadImage.ts
@@ -0,0 +1,38 @@
+'use server'
+
+import * as objectstorage from 'oci-objectstorage'
+import * as common from 'oci-common'
+
+export const uploadImage = async (formData: FormData, username: string) => {
+  const provider: common.ConfigFileAuthenticationDetailsProvider =
+    new common.ConfigFileAuthenticationDetailsProvider()
+
+  ;(async () => {
+    try {
+      // Create a service client
+      const client = new objectstorage.ObjectStorageClient({
+        authenticationDetailsProvider: provider,
+      })
+
+      const file = formData.get('file') as File
+
+      const buffer = await file.arrayBuffer()
+      const fileContent = new Uint8Array(buffer)
+
+      const putObjectRequest = {
+        namespaceName: process.env.NAMESPACE as string,
+        bucketName: process.env.BUCKET_NAME as string,
+        objectName: `pfps/pfp_${username}.${file.type.split('/')[1]}`,
+        putObjectBody: fileContent,
+        contentType: file.type,
+        contentLength: file.size,
+      }
+
+      // Send request to the Client.
+      const res = await client.putObject(putObjectRequest)
+      console.log(putObjectRequest)
+    } catch (error) {
+      console.log('put Failed with error  ' + error)
+    }
+  })()
+}
