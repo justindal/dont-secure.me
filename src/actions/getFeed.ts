d82335178743c3314ad417e00f359ccab670335e
justindal <justindaludado@cmail.carleton.ca>
2024-07-29 01:54:56 -0400

    getFeed server action implemented

--- src/actions/getFeed.ts
+++ src/actions/getFeed.ts
@@ -1,1 +1,46 @@
 'use server'
+
+import db from '@/lib/db'
+import { auth } from '@/auth'
+
+const getFeed = async (page = 1, limit = 20) => {
+  const session = await auth()
+  if (!session) {
+    return {
+      error: 'Not authenticated',
+    }
+  }
+
+  const client = await db.clientPromise
+  const database = client.db(process.env.DB_NAME)
+  const posts = database.collection('posts')
+
+  const getFollowingList = async () => {
+    const following = await database
+      .collection('following')
+      .find({
+        user: session.user._id,
+      })
+      .toArray()
+    return following.map((f) => f.following)
+  }
+
+  const feed = await posts
+    .aggregate([
+      { $match: { user: { $in: await getFollowingList() } } },
+      { $sort: { date: -1 } },
+      { $skip: (page - 1) * limit },
+      { $limit: limit },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'user',
+          foreignField: '_id',
+          as: 'userDetails',
+        },
+      },
+      { $unwind: '$userDetails' },
+    ])
+    .toArray()
+  return feed
+}
