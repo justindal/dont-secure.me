d139710936585aebcba1e7678da9c179a597ea46
justindal <justindaludado@cmail.carleton.ca>
2024-08-12 01:19:22 -0400

    profile page

---
+++ src/app/user/profile/page.tsx
@@ -0,0 +1,43 @@
+import { auth } from '@/auth'
+
+import HomeNav from '@/components/homenavigation'
+import HomeSuggestedCard from '@/components/suggestedcard'
+import ProfileHead from '@components/profilehead'
+import Panel from '@/components/userpanel'
+import Feed from '@/components/feed'
+import { Separator } from '@/components/ui/separator'
+import { UnauthorizedAlert } from '@/components/unauthorized'
+
+const page = async () => {
+  const session = await auth()
+  console.log(session)
+  if (!session) return UnauthorizedAlert()
+
+  return (
+    <div>
+      <HomeNav></HomeNav>
+      <main>
+        <div className='flex justify-center space-x-12 pt-1'>
+          <div className='w-[350px]'>
+            <HomeSuggestedCard></HomeSuggestedCard>
+          </div>
+          <div>
+            <Separator orientation='vertical'></Separator>
+          </div>
+          <div>
+            <ProfileHead></ProfileHead>
+            <Feed feedType='user' feedUsername={session.user.username}></Feed>
+          </div>
+          <div>
+            <Separator orientation='vertical'></Separator>
+          </div>
+          <div className='w-[350px]'>
+            <Panel session={session}></Panel>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default page
