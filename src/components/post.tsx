2448a4d9f065c91c155a2e574c8bab5b26d890db
justindal <justindaludado@cmail.carleton.ca>
2024-08-18 03:59:18 -0400

    update post to include profile picture

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import {
   Card,
   CardContent,
@@ -12,6 +14,7 @@ import { Separator } from './ui/separator'
 
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import Link from 'next/link'
+import { getProfilePicture } from '@/actions/profilepicture/getProfilePicture'
 
 interface PostProps {
   username: string
@@ -22,15 +25,37 @@ interface PostProps {
 }
 
 const Post = ({ username, title, textContent, date }: PostProps) => {
+  const [imageURL, setImageURL] = useState<string | undefined>(undefined)
+  const fetchProfilePicture = async () => {
+    try {
+      const base64Image = await getProfilePicture(username)
+      if (base64Image) {
+        setImageURL(base64Image)
+      }
+    } catch (error) {
+      console.error('Error fetching profile picture:', error)
+    }
+  }
+  useEffect(() => {
+    fetchProfilePicture()
+  }, [])
   return (
     <Card className='mb-3 ml-3 mr-3'>
-      <div className='pt-2 pl-2 pb-1'>
-        {username && (
-          <div>
-            <Link className='font-semibold' href={`/user/${username}`}>@{username}</Link>
-            {title && ': ' + title}
-          </div>
-        )}
+      <div className='flex items-center justify-between p-4'>
+        <div className='pt-2 pl-2 pb-1'>
+          {username && (
+            <div>
+              <Link className='font-semibold' href={`/user/${username}`}>
+                @{username}
+              </Link>
+              {title && ': ' + title}
+            </div>
+          )}
+        </div>
+        <Avatar className='h-12 w-12 ml-4'>
+          <AvatarImage src={imageURL} alt='Profile Picture' />
+          <AvatarFallback>:)</AvatarFallback>
+        </Avatar>
       </div>
       <Separator className=''></Separator>
 
