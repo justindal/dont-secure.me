06ee13f2b80e20593db08bb33ed0e188ea447acf
justindal <justindaludado@cmail.carleton.ca>
2024-10-18 16:32:54 -0400

    handle cookies when logging out

--- src/actions/login/logout.ts
+++ src/actions/login/logout.ts
@@ -1,9 +1,16 @@
 'use server'
 
 import { signOut } from '@/auth'
+import { NextResponse } from 'next/server'
 
 export const logout = async () => {
   await signOut({
     redirectTo: '/',
   })
+
+  // Clear cookies
+  const response = NextResponse.redirect('/')
+  response.cookies.set('authjs.session-token', '', { path: '/', expires: new Date(0) })
+  response.cookies.set('authjs.csrf-token', '', { path: '/', expires: new Date(0) })
+  return response
 }
