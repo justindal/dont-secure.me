254e14eedd0138b18c43dc74ab14e5a1e591d867
justindal <justindaludado@cmail.carleton.ca>
2024-10-21 16:54:55 -0400

    fix for duplicate users

--- src/app/api/user/createUser/route.ts
+++ src/app/api/user/createUser/route.ts
@@ -1,9 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
 import createUser from '@/actions/user/createUser'
+import checkUser from '@/actions/user/checkUser'
 
 export async function POST(req: NextRequest) {
   try {
     const { username, name } = await req.json()
+    if (!username) {
+      return NextResponse.json(
+        { error: 'Username is required' },
+        { status: 400 },
+      )
+    }
+    const user = await checkUser(username)
+    if (user) {
+      return NextResponse.json(
+        { error: 'Username already exists' },
+        { status: 400 },
+      )
+    }
     const newUser = await createUser(username, name)
     return NextResponse.json(newUser, { status: 201 })
   } catch (error: any) {
