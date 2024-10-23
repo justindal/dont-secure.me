06f2dba7940ab03c726a3edfc9428452963cc542
justindal <justindaludado@cmail.carleton.ca>
2024-10-23 16:25:47 -0400

    adjust login route response

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,32 +2,34 @@ import { auth, signIn } from '@/auth'
 import { NextResponse } from 'next/server'
 
 export const POST = auth(async function POST(req) {
-  // if user is already logged in, return user
-  if (await auth()) {
-    return NextResponse.json({ success: true, user: await auth() })
+  // if user is already logged in, return user and session
+  const session = await auth()
+  if (session) {
+    return NextResponse.json({ success: true, user: session.user, session })
   }
 
   try {
     const { username } = await req.json()
-    const user = await signIn('credentials', {
+    const result = await signIn('credentials', {
       username: username,
+      redirect: false,
     })
-    if (user) {
-      return NextResponse.json({ success: true, user })
+
+    if (result && 'error' in result && !result.error) {
+      const newSession = await auth()
+      return NextResponse.json({ success: true, user: newSession?.user, session: newSession })
     } else {
       return NextResponse.json(
         { success: false, message: 'Invalid credentials' },
         { status: 401 },
       )
     }
-  } catch (error) {
-    // if NEXT__REDIRECT is thrown, throw
-    if (error instanceof Error && error.message.includes('NEXT_REDIRECT')) {
-      throw error
-    }
+  } catch (error: any) {
+    console.error(error)
     return NextResponse.json(
       { success: false, message: 'Internal Server Error' },
       { status: 500 },
     )
   }
+
 })
